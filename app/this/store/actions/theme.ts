5cfaf01f672bc26c69487e02f3a516f50aac66f1
dev-kota <kotahistorika@gmail.com>
2024-12-19 18:57:22 -0500

    Added theme customization

--- app/this/store/actions/theme.ts
+++ app/this/store/actions/theme.ts
@@ -1,6 +1,6 @@
 export const THEME_CHANGE = "THEME_CHANGE";
 
-export const themeChange = (theme: string) => {
+export const themeChange = (theme: any) => {
   return {
     type: THEME_CHANGE,
     payload: theme,
--- app/this/store/reducers/index.ts
+++ app/this/store/reducers/index.ts
@@ -2,12 +2,13 @@ import { combineReducers } from 'redux';
 import languageReducer from './language';
 import authReducer from './auth';
 import sidebarReducer from './sidebar';
+import themeReducer from './theme';
 
 const rootReducer = combineReducers({
   language: languageReducer,
   auth: authReducer,
   sidebar: sidebarReducer,
-  
+  theme: themeReducer
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
